c658954a518e80538e19911ca387ea9b39e71376
PaulosK <pkpaulosk@gmail.com>
2020-07-24 23:40:12 +0300

    d

--- game/code/enemy.gd
+++ game/code/enemy.gd
@@ -19,14 +19,20 @@ func _ready():
 		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
 		if result.collider:
 			if result.collider.is_in_group("Player"):
+					print("f")
 					var bullet = Bullet.instance()
 					Parent.add_child(bullet)
 					var pos = Camera.unproject_position(global_transform.origin)
 					var bullet_translation_vector = Vector3(global_transform.origin.x +0.2,global_transform.origin.y, global_transform.origin.z)
-	
-					var bs_vc = mouse_position - pos
-					var bullet_speed_vector = Vector3(bs_vc.x, bs_vc.y, 0)
+					
+					var spat = get_node("/root/level/Spatial/Area")
+					var spatial_pos=spat.global_transform.origin
+					bullet_spawn_location = Vector3(spatial_pos.x,spatial_pos.y, 0)
+					
+					
+					var bullet_speed_vector = mouse_position - bullet_spawn_location
 					bullet_speed_vector.y *= -1
+
 	
 					bullet.global_rotate(Vector3(1, 0, 0), 300)
 					bullet.set_speed(bullet_speed_vector.normalized())
@@ -36,13 +42,10 @@ func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
 		target= body
 		print(1)
-
-
 func _on_Area_body_exited(body):
 	if body.is_in_group("Player"):
 		target= null
 		print(2)
-
 
 
 
