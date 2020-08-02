1a29989bee02c91de243c72f038f21e39859612a
PaulosK <pkpaulosk@gmail.com>
2020-08-02 15:43:29 +0300

    explosive

--- game/code/explosive.gd
+++ game/code/explosive.gd
@@ -3,3 +3,17 @@ extends StaticBody
 func explode():
 	print("boom")
 	$Area6/Particles.set_emitting(true)
+	$StaticBody/CollisionShape.disabled = false
+	$Timer.start()
+	$Timer2.start()
+
+func _on_Timer_timeout():
+	$StaticBody/CollisionShape.disabled = true
+	$MeshInstance2.hide()
+	$CollisionShape.disabled=true
+	$Area.show()
+	$Area/CollisionShape.disabled=true
+
+
+func _on_Timer2_timeout():
+	$Area6/Particles.hide()
