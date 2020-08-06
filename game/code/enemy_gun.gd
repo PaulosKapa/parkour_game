37ee76d275841196630f13c89c417f090f38f417
PaulosK <pkpaulosk@gmail.com>
2020-08-06 22:41:55 +0300

    reload

--- game/code/enemy_gun.gd
+++ game/code/enemy_gun.gd
@@ -4,20 +4,34 @@ onready var Camera = get_node("/root/level/InterpolatedCamera")
 onready var Bullet= preload("res://scenes/bullet.tscn")
 onready var mouse_position = Vector3(0,0,0)
 var bullet_spawn_location = Vector3(0,0,0)
+var cant_shoot=0
 
+func cant_shoot():
+	cant_shoot=1
+	return cant_shoot
+	
 func shoot():
-	var bullet = Bullet.instance()
-	Parent.add_child(bullet)
+	if cant_shoot==0:
+		var bullet = Bullet.instance()
+		Parent.add_child(bullet)
 	
-	var spat = get_node("/root/level/Spatial/StaticBody/Spatial")
-	var spatial_pos=spat.global_transform.origin
-	bullet_spawn_location = Vector3(spatial_pos.x,spatial_pos.y, 0)
+		var spat = get_node("/root/level/Spatial/StaticBody/Spatial")
+		var spatial_pos=spat.global_transform.origin
+		bullet_spawn_location = Vector3(spatial_pos.x,spatial_pos.y, 0)
 	
-	var pos = Camera.unproject_position(global_transform.origin)
-	var bullet_translation_vector = Vector3(global_transform.origin.x +2,global_transform.origin.y, global_transform.origin.z)
+		var pos = Camera.unproject_position(global_transform.origin)
+		var bullet_translation_vector = Vector3(global_transform.origin.x +2,global_transform.origin.y, global_transform.origin.z)
 	
-	var bullet_speed_vector = Vector3(1, 0, 0);
+		var bullet_speed_vector = Vector3(1, 0, 0);
 	
-	bullet.global_rotate(Vector3(1, 0, 0), 300)
-	bullet.set_speed(bullet_speed_vector.normalized())
-	bullet.global_translate(bullet_translation_vector)
+		bullet.global_rotate(Vector3(1, 0, 0), 300)
+		bullet.set_speed(bullet_speed_vector.normalized())
+		bullet.global_translate(bullet_translation_vector)
+		$Timer.start()
+		
+		cant_shoot()
+		
+
+func _on_Timer_timeout():
+	cant_shoot=0
+	$Timer.stop()
--- game/code/gun.gd
+++ game/code/gun.gd
@@ -4,22 +4,47 @@ onready var Camera = get_node("/root/level/InterpolatedCamera")
 onready var Bullet= preload("res://scenes/bullet.tscn")
 onready var mouse_position = Vector3(0,0,0)
 var bullet_spawn_location = Vector3(0,0,0)
+var cant_shoot = 0
+var bullets_left=9
 
+func cant_shoot():
+	cant_shoot=1
+	return cant_shoot
 func _process(delta):
-	if Input.is_action_just_pressed("click"):
+	if Input.is_action_pressed("click"):
 		mouse_position = get_tree().root.get_mouse_position()
 		shoot()
 		
 func shoot():
-	var bullet = Bullet.instance()
-	Parent.add_child(bullet)
-	var pos = Camera.unproject_position(Parent.global_transform.origin)
-	var bullet_translation_vector = global_transform.origin
+	if bullets_left<=0:
+			$reload.start()
+			cant_shoot()
+	elif cant_shoot==0:
+		print(bullets_left)
+		var bullet = Bullet.instance()
+		Parent.add_child(bullet)
+		var pos = Camera.unproject_position(Parent.global_transform.origin)
+		var bullet_translation_vector = global_transform.origin
 	
-	var bs_vc = mouse_position - pos
-	var bullet_speed_vector = Vector3(bs_vc.x, bs_vc.y, 0)
-	bullet_speed_vector.y *= -1
+		var bs_vc = mouse_position - pos
+		var bullet_speed_vector = Vector3(bs_vc.x, bs_vc.y, 0)
+		bullet_speed_vector.y *= -1
 	
-	bullet.global_rotate(Vector3(1, 0, 0), 300)
-	bullet.set_speed(bullet_speed_vector.normalized())
-	bullet.global_transform.origin = bullet_translation_vector
+		bullet.global_rotate(Vector3(1, 0, 0), 300)
+		bullet.set_speed(bullet_speed_vector.normalized())
+		bullet.global_transform.origin = bullet_translation_vector
+		bullets_left=bullets_left-1
+		$Timer.start()
+		cant_shoot()
+
+func _on_Timer_timeout():
+	cant_shoot=0
+	$Timer.stop()
+
+
+func _on_reload_timeout():
+	
+	bullets_left=9
+	cant_shoot=0
+	print(bullets_left)
+	$reload.stop()
