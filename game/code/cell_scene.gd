df05180f8de5f7b458725a72bf317748bcca677a
PaulosK <pkpaulosk@gmail.com>
2020-12-31 14:34:37 +0200

    code and scene changes

--- game/code/cell_scene.gd
+++ game/code/cell_scene.gd
@@ -2,16 +2,21 @@ extends Spatial
 
 func _ready():
 	$lights_roof/onoff/OmniLight.hide()
+	$lights_roof/onoff/Cylinder001.get_surface_material(0).set_emission_energy(0)
 	$lights_roof/onoff/onoff.start()
 	
 func _on_onoffdown_timeout():
 	$lights_roof/onoffdown/OmniLight.show()
+	$lights_roof/onoffdown/Cylinder001.get_surface_material(0).set_emission_energy(1)
 	$lights_roof/onoffdown/onoffdown.stop()
 	$lights_roof/onofffinal/OmniLight.hide()
+	$lights_roof/onofffinal/Cylinder001.get_surface_material(0).set_emission_energy(0)
 	$lights_roof/onofffinal/onofffinal.start()
 	
 func _on_onofffinal_timeout():
 	$lights_roof/onofffinal/OmniLight.show()
+	$lights_roof/onofffinal/Cylinder001.get_surface_material(0).set_emission_energy(1)
+	$lights_roof/onoff/Cylinder001.get_surface_material(0).set_emission_energy(0)
 	$lights_roof/onofffinal/onofffinal.stop()
 	$lights_roof/onoff/OmniLight.hide()
 	$lights_roof/onoff/onoff.start()
@@ -19,6 +24,8 @@ func _on_onofffinal_timeout():
 func _on_onoff_timeout():
 	$lights_roof/onoff/OmniLight.show()
 	$lights_roof/onoffdown/OmniLight.hide()
+	$lights_roof/onoff/Cylinder001.get_surface_material(0).set_emission_energy(1)
+	$lights_roof/onoffdown/Cylinder001.get_surface_material(0).set_emission_energy(0)
 	$lights_roof/onoffdown/onoffdown.start()
 	$lights_roof/onoff/onoff.stop()
 	
--- game/code/door.gd
+++ game/code/door.gd
@@ -3,7 +3,7 @@ var unl_door=0
 
 func _on_Area5_body_entered(body):
 	if unl_door==1 and body.is_in_group("Player"):
-		$AnimationPlayer.play("door")
+		$".".hide()
 
 func _on_key_unlock():
 	unl_door=1
