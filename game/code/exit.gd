4cd0c361f388f9b8a875cce7f3d6cb30b4407d10
Floyd Johnson <fljohnson3@sisna.com>
2021-03-07 17:57:59 -0500

    moved load of ui_scene.tscn from exit.gd::onready to within the only function it's used in.

--- game/code/exit.gd
+++ game/code/exit.gd
@@ -1,5 +1,6 @@
 extends Area
-onready var start_ui = load("res://scenes/start_ui.tscn")
+
+
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
@@ -8,5 +9,10 @@ func _on_Area_body_entered(body):
 		#Both calls probably should be replaced by a single "body.health = 0"
 		#(FLJ, 9/1/2020)
 		GameData.restore("user://saves")
+		#1. As an onready, this load was fouling up the game start
+		#2. The only time exit.gd uses it is here
+		#As such, it seemed better to move it to here instead of the onready where it was causing delays
+		#(FLJ, 3/7/2021)
+		var start_ui = load("res://scenes/start_ui.tscn")
 		var _ret = Global.goto_scene(start_ui)
 
