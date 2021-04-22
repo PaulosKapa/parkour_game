506d8632a672c260a714d0535e55759d64d88356
Floyd Johnson <fljohnson3@sisna.com>
2021-04-22 11:18:05 -0400

    Commented out the secondary response to click in 1st_cutscene.gd

--- game/code/1st_cutscene.gd
+++ game/code/1st_cutscene.gd
@@ -1,8 +1,8 @@
 extends Node2D
 onready var next_scene = preload("res://scenes/cell_scene_final.tscn")
-func _ready():
-	if Input.is_action_just_pressed("click"):
-		get_tree().change_scene("res://scenes/cell_scene_final.tscn")
+#func _ready():
+#	if Input.is_action_just_pressed("click"):
+#		get_tree().change_scene("res://scenes/cell_scene_final.tscn")
 
 func _on_VideoPlayer_finished():
 	Global.goto_scene(next_scene)
