24819df11fc5734858556634ac3aa5e1eb806ecb
Floyd Johnson <fljohnson3@sisna.com>
2021-04-22 14:27:02 -0400

    added ability to skip the expository cutscene by pressing space bar

--- game/code/1st_cutscene.gd
+++ game/code/1st_cutscene.gd
@@ -6,3 +6,12 @@ onready var next_scene = preload("res://scenes/cell_scene_final.tscn")
 
 func _on_VideoPlayer_finished():
 	Global.goto_scene(next_scene)
+
+
+func _process(delta):
+	if !($VideoPlayer.is_playing()):
+		return
+	if(Input.is_key_pressed(KEY_SPACE)):
+		self._on_VideoPlayer_finished()
+	
+		
